6c1703057a20af6b9fd8f406e87d6539249a5224
Maximus Juul <Maximusjjuul@gmail.com>
2022-05-11 20:51:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,16 +9,30 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// When the client is ready, run this code (only once)
+// client.once('ready', () => {
+// 	console.log('Ready!');
+// });
+
+// When a user runs a command
 client.on('interactionCreate', async interaction => {
 	if (!interaction.isCommand()) return;
 
